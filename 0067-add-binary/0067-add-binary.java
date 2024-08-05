0189e7e71bdb0d31c8ba964f5626964126efc8ac
Luizse <88157931+Luizse@users.noreply.github.com>
2024-08-05 17:57:50 +0530

    Time: 1 ms (99.92%), Space: 42.1 MB (65.81%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        //a = "1010", b = "1011"
+       StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0)
+                carry += a.charAt(i--) - '0';
+            if (j >= 0)
+                carry += b.charAt(j--) - '0';
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+       
+}
