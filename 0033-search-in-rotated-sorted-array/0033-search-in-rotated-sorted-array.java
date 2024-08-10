4c48821730ee9e221cd60c74d41a2b3c72337864
Luizse <88157931+Luizse@users.noreply.github.com>
2024-08-10 18:56:34 +0530

    Time: 0 ms (100%), Space: 42 MB (46.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,35 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = 0;
+       
+        int low = 0, high = nums.length - 1;
 
-        // Find the rotation pivot where the array rotation happened
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                pivot = i;
-                break;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
 
-        // If the target is at the pivot point, return the pivot
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        // Search the left part of the array (from pivot+1 to the end)
-        for (int i = pivot + 1; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
-            }
-        }
-
-        // Search the right part of the array (from the start to pivot)
-        for (int i = 0; i <= pivot; i++) {
-            if (nums[i] == target) {
-                return i;
-            }
-        }
-
-        // If the target is not found, return -1
         return -1;
     }
 }
